22813f50ed4afc1611e899530882d0878c67195d
Brady Sutton <bsutt123@gmail.com>
2018-02-01 10:36:33 -0500

    added in attrs support from updated master branch

--- test.js
+++ test.js
@@ -133,6 +133,23 @@ const media = Object.keys(sizes).reduce((acc, label) => {
     }
   `
 })
+const Input = styled.input.attrs({
+  // we can define static props
+  type: 'password',
+
+  // or we can define dynamic ones
+  margin: props => props.size || '1em',
+  padding: props => props.size || '1em'
+})`
+  color: palevioletred;
+  font-size: 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+
+  /* here we use the dynamically computed props */
+  margin: ${props => props.margin};
+  padding: ${props => props.padding};
+`;
 
 const mediaQuery = styled.div`
   background: palevioletred;
@@ -149,3 +166,50 @@ const mediaQuery = styled.div`
     background: coral;
   `};
 `
+const Input = styled.input.attrs({
+  // we can define static props
+  type: 'password',
+
+  // or we can define dynamic ones
+  margin: props => props.size || '1em',
+  padding: props => props.size || '1em'
+})`
+  color: palevioletred;
+  font-size: 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+
+  /* here we use the dynamically computed props */
+  margin: ${props => props.margin};
+  padding: ${props => props.padding};
+`;
+
+const Input = styled.input.attrs({
+  // we can define static props
+  type: 'password',
+
+  // or we can define dynamic ones
+  margin: props => props.size || '1em',
+  padding: props => props.size || '1em'
+})`
+  color: palevioletred;
+  font-size: 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+
+  ${media.desktop`
+    background: red;
+  `};
+
+  ${media.breakAt('300px')`
+    background: palevioletred;
+  `}
+
+  ${media.desktop(400)`
+    background: coral;
+  `};
+
+  /* here we use the dynamically computed props */
+  margin: ${props => props.margin};
+  padding: ${props => props.padding};
+`;
