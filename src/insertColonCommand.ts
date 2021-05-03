efa9190a0f4e21f5d50d99292298f4af3b8696a0
Jason Williams <jase.williams@gmail.com>
2021-05-03 18:12:59 +0100

    * Depend on css-language-server for CSS Data * Create a command to insert colon or semi colon * If the property value ends in ) move cursor back

---
+++ src/insertColonCommand.ts
@@ -0,0 +1,37 @@
+import { commands, window } from "vscode";
+import { getDefaultCSSDataProvider } from "vscode-css-languageservice";
+
+const cssDataProvider = getDefaultCSSDataProvider();
+const properties = cssDataProvider.provideProperties();
+
+export const enterKeyEvent = commands.registerCommand(
+  "extension.insertColonOrSemiColon",
+  async () => {
+    await commands.executeCommand("acceptSelectedSuggestion");
+    const editor = window.activeTextEditor;
+
+    if (!editor) {
+      return;
+    }
+
+    const cursorPosition = editor.selection;
+    const lineText = editor.document.lineAt(cursorPosition.start.line).text;
+    const lineTextList = lineText.trim().split(" ");
+    const lastWordBeforeCursor = lineTextList[lineTextList.length - 1];
+
+    if (properties.find((value) => value.name === lastWordBeforeCursor)) {
+      editor.edit((editBuilder) => {
+        editBuilder.insert(
+          editor.document.lineAt(cursorPosition.active).range.end,
+          ": ;"
+        );
+        commands.executeCommand("cursorLeft");
+        commands.executeCommand("editor.action.triggerSuggest");
+      });
+    }
+
+    if (lastWordBeforeCursor[lastWordBeforeCursor.length - 2] === ")") {
+      commands.executeCommand("cursorLeft");
+    }
+  }
+);
