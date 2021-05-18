5ee47253c3b22695043c65e5c9f70bffd1871703
Jason Williams <jase.williams@gmail.com>
2021-05-18 19:02:31 +0100

    Support NextJS style tags

---
+++ src/tests/suite/colorize-fixtures/nextStyle.js
@@ -0,0 +1,14 @@
+function Home() {
+    return (
+      <div className="container">
+        <style jsx>{`
+          .container {
+            margin: 50px;
+          }
+          p {
+            color: blue;
+          }
+        `}</style>
+      </div>
+    )
+}
