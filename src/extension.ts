2059975990e5948fc5b6b24671c4fbbd3bd174d6
Jason Williams <jase.williams@gmail.com>
2021-07-24 15:52:02 +0100

    Check for conflicting extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,10 @@
-import { ExtensionContext, languages, DocumentSelector } from "vscode";
+import {
+  ExtensionContext,
+  languages,
+  extensions,
+  DocumentSelector,
+  window,
+} from "vscode";
 import { colorProvider } from "./colorProvider";
 import { provideCompletionItems } from "./completionItemProvider";
 import { enterKeyEvent } from "./insertColonCommand";
@@ -23,6 +29,14 @@ function activate(context: ExtensionContext) {
 
   // Handle auto Colon / SemiColon
   context.subscriptions.push(enterKeyEvent);
+
+  // Check for conflicting extensions #310
+  const result = extensions.all.find((v) => v.id === "blanu.vscode-styled-jsx");
+  if (result) {
+    window.showErrorMessage(
+      `You have ${result.packageJSON.name} installed. This conflicts with the VSCode Styled Components extension and is not needed, you can remove or disable it`
+    );
+  }
 }
 
 module.exports = {
